c615836fdb65b0a1373ebc998c18ab45877831a5
Ronak Shah <ronakshahultimate@gmail.com>
2016-08-14 13:52:05 -0700

    Initial Commit

---
+++ iTunesConnect/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iTunesConnect
+//
+//  Created by Ronak Shah on 8/14/16.
+//  Copyright © 2016 Shah Industries. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iTunesConnect/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iTunesConnect
+//
+//  Created by Ronak Shah on 8/14/16.
+//  Copyright © 2016 Shah Industries. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
